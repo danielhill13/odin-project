707f8a1d85f59d2fb0f749f3f49038136455bc70
Daniel Hill <daniel.hill13@gmail.com>
2016-11-03 13:20:32 -0700

    Update euler-problem-3.js

--- js-lesson/euler-problem-3.js
+++ js-lesson/euler-problem-3.js
@@ -1,1 +1,19 @@
+//This causes browser to timeout - beware!
+//New to Javascript - this works, but will timeout the browser as it is inefficient. I'll come back to update it later
 
+var numtest = 600851475143;
+var array = [];
+
+for (i = 1; i <= numtest; i++) {
+	if (numtest % i === 0) {
+		array.push(i);
+	}	else{}
+}
+
+var largest = 0;
+for (i = 0; i <= largest; i++) {
+	if (array[i] > largest) {
+		largest = array[i];
+	}
+}
+console.log(largest);
