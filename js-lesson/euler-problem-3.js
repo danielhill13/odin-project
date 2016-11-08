c524742f72d2503db586cff98b35b088c2bd361a
Daniel Hill <daniel.hill13@gmail.com>
2016-11-08 13:24:32 -0800

    Update euler-problem-3.js

--- js-lesson/euler-problem-3.js
+++ js-lesson/euler-problem-3.js
@@ -1,19 +1,3 @@
-//This causes browser to timeout - beware!
-//This does not work.
+//What is the largest prime factor of the number 600851475143 ?
 
 var numtest = 600851475143;
-var array = [];
-
-for (i = 1; i <= numtest; i++) {
-	if (numtest % i === 0) {
-		array.push(i);
-	}	else{}
-}
-
-var largest = 0;
-for (i = 0; i <= largest; i++) {
-	if (array[i] > largest) {
-		largest = array[i];
-	}
-}
-console.log(largest);
