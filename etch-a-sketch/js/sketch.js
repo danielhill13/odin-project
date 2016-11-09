9dc4a080ce8054808732fbc8807eae3061840a25
Daniel Hill <daniel.hill13@gmail.com>
2016-11-09 11:24:50 -0800

    working on clear functionality

--- etch-a-sketch/js/sketch.js
+++ etch-a-sketch/js/sketch.js
@@ -1,16 +1,24 @@
-for (i = 0; i < 32; i++) {
+var i = 0;
+function writetable() {
+	for (i = 0; i < 32; i++) {
 	$('#gridwrapper').append('<tr class="etchrow"></tr>');
-};
-for (j = 0; j < 32; j++) {
+}	for (i = 0; i < 32; i++) {
 	$('.etchrow').append('<td class="etchtabledata"></td>');
 };
+};
+writetable();
+
 
 $('.etchtabledata').hover(function() {
 	$(this).css('background-color', '#aaa')
 
 });
 
+//need to work on this. it shrinks the size every clear
 $('#clear').click(function() {
-	$('.etchtabledata').css('background-color', 'initial')
+	$('.etchtabledata').css('background-color', 'initial');
+	var usergridinput = prompt("How big do you want the grid?");
+	i = usergridinput;
+	writetable();
 
 });
