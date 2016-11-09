3156d5074c0fbc8d5e358eebdcdea1c3573708bd
Daniel Hill <daniel.hill13@gmail.com>
2016-11-09 15:23:18 -0800

    got the rewrite working

--- etch-a-sketch/js/sketch.js
+++ etch-a-sketch/js/sketch.js
@@ -1,24 +1,29 @@
-var i = 0;
-function writetable() {
-	for (i = 0; i < 32; i++) {
+var initialGrid = 32
+//Writes and rewrites the table cells
+function writeTable() {
+	for (i = 0; i < initialGrid; i++) {
 	$('#gridwrapper').append('<tr class="etchrow"></tr>');
-}	for (i = 0; i < 32; i++) {
+}	for (i = 0; i < initialGrid; i++) {
 	$('.etchrow').append('<td class="etchtabledata"></td>');
+	var cellHeight = 640 / initialGrid;
+	var cellWidth = 640 / initialGrid;	
+	$('.etchtabledata').css({'height':cellHeight, 'width':cellWidth})
 };
 };
-writetable();
+writeTable();
 
+//Adds shading functionality
+$(document).on('mouseover', '.etchtabledata', function() {
+	$(this).css('background-color', '#aaa');
+})
 
-$('.etchtabledata').hover(function() {
-	$(this).css('background-color', '#aaa')
-
-});
-
-//need to work on this. it shrinks the size every clear
+//Actionizes Clear button, prompts for new grid size, gets new grid
 $('#clear').click(function() {
-	$('.etchtabledata').css('background-color', 'initial');
-	var usergridinput = prompt("How big do you want the grid?");
-	i = usergridinput;
-	writetable();
+	$('.etchrow').remove();
+	$('.etchtabledata').remove();
+	var newGrid = prompt("How big do you want the grid?");
+	console.log(newGrid);
+	initialGrid = newGrid;
+	writeTable();
 
 });
