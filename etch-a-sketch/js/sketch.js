1b5b2d9b12412309f6ce8288d8295a8ed32bc3d9
Daniel Hill <daniel.hill13@gmail.com>
2016-11-09 09:07:26 -0800

    got the divs created! now to the clear button..

--- etch-a-sketch/js/sketch.js
+++ etch-a-sketch/js/sketch.js
@@ -1,6 +1,11 @@
-var $newdiv = $('<div />').addClass('etchdiv');
 
-
-for (var i = 0; i < 17; i++) {
-	$("#gridwrapper").append($newdiv);
+for (j = 0; j < 16; j++) {
+	for (i = 0; i < 16; i++) {
+	$('#gridwrapper').append('<div class="etchdiv"></div>');
+	};
+	$('#gridwrapper').append('<br/>');
 };
+$('.etchdiv').hover(function() {
+	$(this).css('background-color', '#aaa')
+
+});
