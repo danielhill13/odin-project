250684dd5f00d661038f723598ae7ad38eeafe30
Daniel Hill <daniel.hill13@gmail.com>
2016-11-09 10:56:28 -0800

    changed divs to table to avoid br space

--- etch-a-sketch/js/sketch.js
+++ etch-a-sketch/js/sketch.js
@@ -1,11 +1,16 @@
-
-for (j = 0; j < 16; j++) {
-	for (i = 0; i < 16; i++) {
-	$('#gridwrapper').append('<div class="etchdiv"></div>');
-	};
-	$('#gridwrapper').append('<br/>');
+for (i = 0; i < 32; i++) {
+	$('#gridwrapper').append('<tr class="etchrow"></tr>');
 };
-$('.etchdiv').hover(function() {
+for (j = 0; j < 32; j++) {
+	$('.etchrow').append('<td class="etchtabledata"></td>');
+};
+
+$('.etchtabledata').hover(function() {
 	$(this).css('background-color', '#aaa')
+
+});
+
+$('#clear').click(function() {
+	$('.etchtabledata').css('background-color', 'initial')
 
 });
