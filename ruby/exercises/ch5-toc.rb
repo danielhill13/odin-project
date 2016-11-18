e2fb61f9f4ddda6c0c5b06ee35ab29289a0521c1
Daniel Hill <daniel.hill13@gmail.com>
2016-11-18 15:32:21 -0800

    table of contents into array

--- ruby/exercises/ch5-toc.rb
+++ ruby/exercises/ch5-toc.rb
@@ -1,5 +1,3 @@
 lineWidth = 50
-puts('Table of Contents'.center(lineWidth))
-puts 'Chapter 1: Numbers'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2)
-puts 'Chapter 2: Letters'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2)
-puts 'Chapter 3: Variables'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2)
+tableOfContents = [('Table of Contents'.center(lineWidth)), 'Chapter 1: Numbers'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2), 'Chapter 2: Letters'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2), 'Chapter 3: Variables'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2)]
+puts tableOfContents
