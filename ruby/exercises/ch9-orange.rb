2b7fefd1d7679168e3d53c5405adbcfc647456dd
Daniel Hill <daniel.hill13@gmail.com>
2016-12-07 11:37:09 -0800

    orangetree exercises complete

--- ruby/exercises/ch9-orange.rb
+++ ruby/exercises/ch9-orange.rb
@@ -1,13 +1,3 @@
-# Make an OrangeTree class. It should have a  height method which returns its height, and a
-# oneYearPasses method, which, when called, ages the tree one year. Each year the tree grows
-# taller (however much you think an orange tree should grow in a year), and after some number
-# of years (again, your call) the tree should die. For the first few years, it should not produce
-# fruit, but after a while it should, and I guess that older trees produce more each year than younger
-# trees... whatever you think makes most sense. And, of course, you should be able to countTheOranges
-# (which returns the number of oranges on the tree), and pickAnOrange (which reduces the @orangeCount
-# by one and returns a string telling you how delicious the orange was, or else it just tells you that
-# there are no more oranges to pick this year). Make sure that any oranges you don't pick one year fall
-# off before the next year.
 class OrangeTree
   def initialize
     @treeHeight = 0
@@ -16,9 +6,14 @@ class OrangeTree
   end
 
   def oneYearPasses
-    if @treeAge < 15
-      # Make fruit not grow first 3 years
-      # 
+    if @treeAge < 4
+      @treeAge = @treeAge + 1
+      @treeHeight = @treeAge * 2
+      puts 'A year has passed...'
+      puts 'The tree is ' + @treeAge.to_s + ' years old.'
+      puts 'The tree is ' + @treeHeight.to_s + ' feet tall.'
+      puts 'This year, the tree bore ' + @fruitBear.to_s + ' oranges for eating.'
+    elsif @treeAge < 15
       @treeAge = @treeAge + 1
       @treeHeight = @treeAge * 2
       @fruitBear = @treeAge * 7
@@ -32,8 +27,12 @@ class OrangeTree
 
   def pickAnOrange
     # subtract 1 from fruitBear
+    if @fruitBear == 0
+      puts 'No more oranges this year!'
+    else
     @fruitBear = @fruitBear - 1
     puts 'After eating a delicious orange from the tree, there are ' + @fruitBear.to_s + ' oranges remaining.'
+  end
   end
 end
 end
@@ -51,6 +50,7 @@ tree.oneYearPasses
 tree.oneYearPasses
 tree.oneYearPasses
 tree.oneYearPasses
+tree.pickAnOrange
 tree.oneYearPasses
 tree.oneYearPasses
 tree.oneYearPasses
